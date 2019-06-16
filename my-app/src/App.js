fa3e70398e921ed2ae57991c724d2230c6fabbe3
WaelAlnadeem <wael.alnadeem88@gmail.com>
2019-06-16 03:21:57 +0200

    adding all-quotes button+ fetch all quotes+ back buton

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
       apiUrl: null,
       author: '',
       text: '',
+      screen: 'home',
       count: 0
     };
   }
@@ -60,22 +61,57 @@ class App extends Component {
     let text = `${this.state.author} - ${this.state.text}`
     window.open('http://twitter.com/share?url=' + encodeURIComponent(url) + '&text=' + encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
   }
+  handleClickAllQuotes = () => {
+    this.setState({
+      screen: 'AllData'
+    })
+  }
+  backHome = () => {
+    this.setState({ screen: 'home' })
+  }
 
   render() {
-    return (
-      <div id="main">
-        <h1 id="title">Random Quote Machine</h1>
-        <div id="quote-box">
-          <p id="text">{this.state.text}</p>
-          <p id="author"> - {this.state.author}</p>
-          <button id="tweet-quote" onClick={this.shareOnTwitter}>Twitter</button>
-          <button id="new-quote" onClick={this.handleClick}>New Quote</button>
-          <Particles
-            params={particleOpt}
-          />
+    const AllQuotes = [];
+    if (this.state.screen === 'home') {
+      return (
+        <div id="main">
+          <h1 id="title">Random Quote Machine</h1>
+          <div id="quote-box">
+            <p id="text">{this.state.text}</p>
+            <p id="author"> - {this.state.author}</p>
+            <button id="tweet-quote" onClick={this.shareOnTwitter}>Twitter</button>
+            <button id="new-quote" onClick={this.handleClick}>New Quote</button>
+            <button id="AllData" onClick={this.handleClickAllQuotes}>Show all quotes</button>
+
+            <Particles
+              params={particleOpt}
+            />
+
+          </div>
+
         </div>
-      </div>
-    );
+      )
+
+    } else if (this.state.screen === 'AllData') {
+
+      this.state.apiUrl.forEach(element => {
+        AllQuotes.push(
+          <div>
+            <ul className="allList">
+              <li className="QuoteList">{"Quote : " + element.quote}</li>
+              <li className="AuthorList">{"author : " + element.author}</li>
+            </ul>
+
+          </div>
+        );
+
+      })
+    }
+    return <div>
+      <h1 id="AllQuotes">All Quotes</h1>
+      {AllQuotes}
+      <button id="backHome" onClick={this.backHome}>Back</button>
+    </div>
   }
 }
 
